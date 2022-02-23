60cb7538c37ee6bb0f01d541aced46347fc91818
Vova Rabeshko <rabeshko23@gmail.com>
2022-02-23 22:54:05 +0200

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,10 +1,10 @@
-const changeColorBtnNode = document.querySelector(".change-color");
-const bodyNode = document.querySelector("body");
-const spanNode = document.querySelector(".color");
-changeColorBtnNode.addEventListener("click", () => {
+const changeColor = document.querySelector(".change-color");
+const bodyType = document.querySelector("body");
+const spanType = document.querySelector(".color");
+changeColor.addEventListener("click", () => {
   const color = getRandomHexColor();
-  bodyNode.style.background = color;
-  spanNode.textContent = color;
+  bodyType.style.background = color;
+  spanType.textContent = color;
 });
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
